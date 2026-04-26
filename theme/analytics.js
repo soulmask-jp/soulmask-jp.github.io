82d1c1e98953107f363ac15f18cd3607ffe3c0f5
musasabin <musasabin@users.noreply.github.com>
2026-04-27 02:09:52 +0900

    [wiki] GA4測定IDをSoulmask用に差し替え (G-HFV90MRY1T)

--- theme/analytics.js
+++ theme/analytics.js
@@ -2,11 +2,11 @@
 (function() {
   var script = document.createElement('script');
   script.async = true;
-  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-TT0VJGD2Z5';
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-HFV90MRY1T';
   document.head.appendChild(script);
 
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('config', 'G-TT0VJGD2Z5');
+  gtag('config', 'G-HFV90MRY1T');
 })();
